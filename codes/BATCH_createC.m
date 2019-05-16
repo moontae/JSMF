5af1b6a34dd75d846e831dc74cd92f75b2720330
Moontae Lee <t-mole@microsoft.com>
2019-05-15 18:44:24 -0700

    Small fixes in BATCH generation

--- codes/BATCH_createC.m
+++ codes/BATCH_createC.m
@@ -3,12 +3,12 @@
 %
 % Coded by: Moontae Lee
 % Examples:
-%   - BATCH_createVD('../../jsmf-dataset/dataset/real_raw', '../../jsmf-dataset/real_mat_comp');
+%   - BATCH_createC('../../jsmf-dataset/dataset/real_raw', '../../jsmf-dataset/real_mat_full');
 %
 
 
 %%
-% Main: BATCH_createVD(input_folder, output_folder)
+% Main: BATCH_createC(input_folder, output_folder)
 % 
 % Inputs:
 %   - input_folder: the folder where _train.mat files exist.
@@ -18,11 +18,8 @@
 %   - This function creates compressed version for large vocabulary dataset
 %     that cannot be processed with the existing algorithms.
 %
-function BATCH_createVD(input_folder, output_folder, min_objects, min_tokens, T)
+function BATCH_createC(input_folder, output_folder, min_objects, min_tokens)
     % Set the default parameters.
-    if nargin < 5
-        T = 50;
-    end
     if nargin < 4
         min_tokens = 5;
     end
@@ -35,39 +32,24 @@ function BATCH_createVD(input_folder, output_folder, min_objects, min_tokens, T)
     Nsets = {[500, 1000, 2000, 4447], ...
              [200, 400, 800, 1606], ...       
              [1250, 2500, 5000, 10000], ...
-             [7500, 15000, 30000, 60000], ...
+             [7500, 15000], ...
              [1250, 2500, 5000, 10000], ...
-             [5000, 10000, 20000, 40000]};               
-    Ks = [5, 10, 15, 20, 25, 50, 75, 100, 125, 150];
-    Ksets = {Ks(1:8), Ks(1:8), Ks(1:8), Ks(1:end), Ks(1:8), Ks(1:8)};           
-    
-%     % Additional settings.
-%     datasets = {'nytimes'};
-%     Nsets = {[7500, 15000, 30000, 60000]};
-%     Ks = [200 250 300];
-%     Ksets = {Ks};    
-
-%     % Additional settings.
-%     datasets = {'songs'};
-%     Nsets = {[5000, 10000, 20000, 40000]};
-%     Ks = [200];
-%     Ksets = {Ks};   
-    
+             [5000, 10000]};               
+        
     % Create the ouput folder it it does not exist yet.
     if ~isfolder(output_folder)
         mkdir(output_folder);
     end
     
     % Prepare a logger to record the result and performance.
-    logger = logging.getLogger('BATCH_createVD_logger', 'path', sprintf('BATCH_createVD.log'));
-    logger.info('BATCH_createVD');
+    logger = logging.getLogger('BATCH_createC_logger', 'path', sprintf('BATCH_createC.log'));
+    logger.info('BATCH_createC');
         
     % For each dataset,        
     for i = 1:length(datasets)        
         % Pick a corresponding option.
         dataset = datasets{i};       
-        Nset = Nsets{i};
-        Kset = Ksets{i};      
+        Nset = Nsets{i};         
         logger.info('+ Start creating rectified and compressed datasets for [%s]...', dataset);
         
         % Prepare the data filenames with respect to the current dataset.
@@ -84,27 +66,22 @@ function BATCH_createVD(input_folder, output_folder, min_objects, min_tokens, T)
             
             % Read the BOW data with pruning the vocabulary.
             [bows, ~] = file.readBows(bowsFilename, dictFilename, stopFilename, N, min_objects, statFilename);                
+                               
+            % Compress the bag-of-words(objects) with respect to the current number of clusters.              
+            startTime = tic;
+            [C, D1, D2] = file.bows2C(bows, min_tokens);             
+            elapsedTime = toc(startTime);
 
-            % For each number of clusters,
-            for K = Kset                       
-                logger.info('    + Constructing with K=%d...', K);
-                                
-                % Compress the bag-of-words(objects) with respect to the current number of clusters.              
-                startTime = tic;
-                [V, D, H] = file.bows2VD(bows, K, T, min_tokens);             
-                elapsedTime = toc(startTime);
-
-                % Save the compressed version.
-                outputFilename = sprintf('%s/%s_N-%d_K-%d.mat', output_folder, dataset, N, K);
-                save(outputFilename, 'V', 'D', 'H');
-                logger.info('      - Saved the rectified and compressd co-occurrence for K=%d!', K);
-                logger.info('      - Elapsed seconds = %.4f', elapsedTime);  
-            end
+            % Save the compressed version.
+            outputFilename = sprintf('%s/%s_N-%d.mat', output_folder, dataset, N);
+            save(outputFilename, 'C', 'D1', 'D2');
+            logger.info('    - Saved the original co-occurrence and (co)example frequencies!');
+            logger.info('    - Elapsed seconds = %.4f', elapsedTime);              
         end
     end
     
     % Close the logger.
-    logging.clearLogger('BATCH_createVD_logger');
+    logging.clearLogger('BATCH_createC_logger');
 end
 
 
