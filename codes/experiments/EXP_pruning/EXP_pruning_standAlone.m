f152f4a213cae9876c4a42620aedb021ab275d16
June Cho <sc782@cornell.edu>
2019-05-17 21:01:49 -0400

    pruning experiment make files

---
+++ codes/experiments/EXP_pruning/EXP_pruning_standAlone.m
@@ -0,0 +1,27 @@
+function EXP_pruning_standAlone(data_folder, dataset, K, p, output_base)
+    % In command syntax, both n and nprocs are strings.
+    if ischar(K) == 1    
+        % Convert a string argument to numeric variable.
+        K = str2double(K);  
+    end
+    
+    if ischar(p) == 1
+        p = str2double(p);
+    end
+
+    % Print the information and call the function.
+    fprintf('Running Matlab stand alone for EXP_pruning...\n');
+    fprintf('- Data folder: %s\n', data_folder);
+    fprintf('- Dataset: %s (K = %d, P = %.2f)\n', dataset, K, p);
+    fprintf('- Output base: %s\n', output_base);
+    EXP_pruning(data_folder, dataset, p, K, output_base);
+
+    % Based on the environment,
+    if isdeployed     
+        % Standalone mode
+        exit;
+    else
+        % Otherwise,
+        close all
+    end
+end   
